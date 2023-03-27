fcb3f28f17ce582e328aa25121a8e1292e03a504
wassidak <wassida.keta@kkumail.com>
2023-03-27 11:00:21 +0700

    Update app.run

--- app.py
+++ app.py
@@ -139,5 +139,5 @@ def about():
     return render_template('about.html')
 
 if __name__=="__main__":
-    # app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
+    # app.run(host="0.0.0.0", port=5000)
