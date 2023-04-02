ee4fa0f75667838b301514756d6eb4ad3970dc1c
wassidak <wassida.keta@kkumail.com>
2023-04-03 00:20:46 +0700

    Update show text train data

--- train_data.py
+++ train_data.py
@@ -43,13 +43,17 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                 if frame_num == 0: 
                     cv2.putText(image, 'STARTING COLLECTION', (120,200), 
                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255, 0), 4, cv2.LINE_AA)
-                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15,12), 
+                    cv2.putText(image, 'Collecting frames for {}'.format(action), (15,20), 
+                               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                    cv2.putText(image, 'Video Number {}'.format(sequence), (15,40), 
                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
                     # Show to screen
                     cv2.imshow('OpenCV Feed', image)
                     cv2.waitKey(2000)
                 else: 
-                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15,12), 
+                    cv2.putText(image, 'Collecting frames for {}'.format(action), (15,20), 
+                               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                    cv2.putText(image, 'Video Number {}'.format(sequence), (15,40), 
                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
                     # Show to screen
                     cv2.imshow('OpenCV Feed', image)
