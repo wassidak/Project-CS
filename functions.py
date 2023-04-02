379f8fd50991d11a4cf0902abb202223d66605d8
wassidak <wassida.keta@kkumail.com>
2023-04-03 00:30:22 +0700

    Setup folders for new data

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-### 1. Import Dependencies
+# 1. Import Dependencies
 import cv2
 import numpy as np
 import os
@@ -7,48 +7,51 @@ import time
 import mediapipe as mp
 import pyparsing
 
-### 2. Keypoints using MP Holistic
-mp_holistic = mp.solutions.holistic # Holistic model
-mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+# 2. Keypoints using MP Holistic
+mp_holistic = mp.solutions.holistic  # Holistic model
+mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
 
 def mediapipe_detection(image, model):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
+    # COLOR CONVERSION BGR 2 RGB
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image.flags.writeable = False                  # Image is no longer writeable
     results = model.process(image)                 # Make prediction
-    image.flags.writeable = True                   # Image is now writeable 
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+    image.flags.writeable = True                   # Image is now writeable
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # COLOR COVERSION RGB 2 BGR
     return image, results
 
 def draw_landmarks(image, results):
-    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS) # Draw face connections
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw left hand connections
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw right hand connections
+    mp_drawing.draw_landmarks(image, results.face_landmarks,mp_holistic.FACEMESH_CONTOURS)  # Draw face connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks,mp_holistic.POSE_CONNECTIONS)  # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks,mp_holistic.HAND_CONNECTIONS)  # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks,mp_holistic.HAND_CONNECTIONS)  # Draw right hand connections
+
 
 def draw_styled_landmarks(image, results):
-    #เปลี่ยนสีของจุด landmark 
+    # เปลี่ยนสีของจุด landmark
     # Draw face connections
     mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS,
-                             mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
-                             mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
-                             )
+                              mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+                              mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
+                              )
     # Draw pose connections
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                             mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=2), 
-                             mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
-                             ) 
+                              mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=2),
+                              mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+                              )
     # Draw left hand connections
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                             mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=2), 
-                             mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
-                             ) 
-    # Draw right hand connections  
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                             mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
-                             mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
-                             ) 
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=2),
+                              mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+                              )
+    # Draw right hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                              mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                              )
 
 # mp_holistic.POSE_CONNECTIONS
+
 
 """
 cap = cv2.VideoCapture(0)
@@ -81,7 +84,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 # draw_landmarks(frame, results)
 # plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
-### 3. Extract Keypoint Values
+# 3. Extract Keypoint Values
 # len(results.left_hand_landmarks.landmark)
 
 # pose = []
@@ -106,22 +109,22 @@ def extract_keypoints(results):
 # np.save('0', result_test)
 # np.load('0.npy')
 
-### 4. Setup Folders for Collection
+# 4. Setup Folders for Collection
 
 # Path for exported data, numpy arrays
-DATA_PATH = os.path.join('Train_30word') 
+DATA_PATH = os.path.join('Train_new30word')
 
 # Actions that we try to detect
 # array ของท่าทางภาษามือ
-actions = np.array(['wait','ขอโทษที่มาสาย','คุณชื่ออะไร','คุณสบายดีไหม','ฉันสบายดี','แล้วพบกันใหม่',
-                    'คุณกินข้าวหรือยัง','คุณอายุเท่าไหร่','ฉันกำลังจะกลับบ้าน','คุณจะกลับบ้านกี่โมง','คุณเหนื่อยไหม',
-                    'ฉันไม่สบาย','พรุ่งนี้คุณจะไปไหน','ห้องน้ำไปทางไหน','ฉันกำลังทานข้าว','ไปกินข้าวด้วยกันไหม',
-                    'คุณทำงานอะไร','ฉันทำงานเป็นช่างทำผม','ฉันทำงานเป็นคนขับรถแท็กซี่','เมื่อวานไปไหนมา',
-                    'ฉันหิวข้าว','เมื่อวานฉันไปเที่ยว','ฉันไปหาหมอ','เดินตรงไป','เลี้ยวซ้าย','เลี้ยวขวา', 
-                    'เดินไปข้างหน้า', 'วันนี้อากาศดี', 'สถานีรถไฟไปทางไหน','ต้องเดินอีกไกลไหม', 'วันนี้ฉันตื่นสาย'])
+actions = np.array(['wait', 'ขอโทษที่มาสาย', 'คุณชื่ออะไร', 'คุณสบายดีไหม', 'ฉันสบายดี',
+                    'แล้วพบกันใหม่', 'คุณกินข้าวแล้วหรือยัง', 'คุณอายุเท่าไหร่', 'ฉันกำลังจะกลับบ้าน', 'คุณจะกลับบ้านกี่โมง',
+                    'คุณเหนื่อยไหม', 'ฉันไม่สบาย', 'พรุ่งนี้คุณจะไปไหน', 'สวัสดี', 'ฉันกำลังทานข้าว',
+                    'ไปกินข้าวด้วยกันไหม', 'คุณทำงานอะไร', 'ฉันทำงานเป็นช่างทำผม', 'ฉันทำงานเป็นคนขับรถแท็กซี่', 'เมื่อวานไปไหนมา',
+                    'ฉันหิวข้าว', 'เมื่อวานฉันไปเที่ยว', 'ฉันไปหาหมอ', 'ฉันขอโทษ', 'ขอบคุณ',
+                    'ฉันไม่เป็นไร', 'โชคดี', 'วันนี้อากาศดี', 'ยินดีที่ได้รู้จัก', 'คุณเข้าใจไหม', 'ฉันไม่เข้าใจ'])
 
 # fifteen videos worth of data
-# 30 video = 30 folder 
+# 30 video = 30 folder
 no_sequences = 30
 
 # Videos are going to be 30 frames in length
