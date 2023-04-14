bb945269b1b5637765cb6c73c54c2e7753983ce5
wassidak <wassida.keta@kkumail.com>
2023-04-15 05:32:54 +0700

    Modified code

--- array_1.py
+++ array_1.py
@@ -3,7 +3,7 @@ import numpy as np
 # print full array 
 np.set_printoptions(threshold=np.inf)
 
-load = np.load('Train_30word/wait/0/0.npy')
+load = np.load('Collect_Data/wait/0/0.npy')
 print(load)
 print(np.max(load))
 print(np.min(load))
