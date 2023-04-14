9f12c5d1daada8ab5262ad03898fff20997ab9c1
wassidak <wassida.keta@kkumail.com>
2023-04-15 05:15:06 +0700

    Update code and comment

--- functions.py
+++ functions.py
@@ -2,22 +2,18 @@
 import cv2
 import numpy as np
 import os
-import matplotlib.pyplot as plt
-import time
 import mediapipe as mp
-import pyparsing
 
 # 2. Keypoints using MP Holistic
 mp_holistic = mp.solutions.holistic  # Holistic model
 mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
 
 def mediapipe_detection(image, model):
-    # COLOR CONVERSION BGR 2 RGB
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
     image.flags.writeable = False                  # Image is no longer writeable
     results = model.process(image)                 # Make prediction
     image.flags.writeable = True                   # Image is now writeable
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # COLOR COVERSION RGB 2 BGR
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
     return image, results
 
 def draw_landmarks(image, results):
@@ -25,7 +21,6 @@ def draw_landmarks(image, results):
     mp_drawing.draw_landmarks(image, results.pose_landmarks,mp_holistic.POSE_CONNECTIONS)  # Draw pose connections
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks,mp_holistic.HAND_CONNECTIONS)  # Draw left hand connections
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks,mp_holistic.HAND_CONNECTIONS)  # Draw right hand connections
-
 
 def draw_styled_landmarks(image, results):
     # เปลี่ยนสีของจุด landmark
@@ -50,53 +45,7 @@ def draw_styled_landmarks(image, results):
                               mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
                               )
 
-# mp_holistic.POSE_CONNECTIONS
-
-
-"""
-cap = cv2.VideoCapture(0)
-# Set mediapipe model 
-with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-    while cap.isOpened():
-
-        # Read feed
-        ret, frame = cap.read()
-
-        # Make detections
-        image, results = mediapipe_detection(frame, holistic)
-        print(results)
-        
-        # Draw landmarks
-        draw_landmarks(image, results)
-
-        # Show to screen
-        cv2.imshow('OpenCV Feed', image)
-
-        # Break gracefully
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
-"""
-
-# len(results.left_hand_landmarks.landmark)
-# results
-# draw_landmarks(frame, results)
-# plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-
 # 3. Extract Keypoint Values
-# len(results.left_hand_landmarks.landmark)
-
-# pose = []
-# for res in results.pose_landmarks.landmark:
-#     test = np.array([res.x, res.y, res.z, res.visibility])
-#     pose.append(test)
-
-# pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
-# lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
-# rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
-# face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
-
 def extract_keypoints(results):
     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
     face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
@@ -104,31 +53,25 @@ def extract_keypoints(results):
     rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
     return np.concatenate([pose,face,lh, rh])
 
-# result_test = extract_keypoints(results)
-# result_test
-# np.save('0', result_test)
-# np.load('0.npy')
-
 # 4. Setup Folders for Collection
-
 # Path for exported data, numpy arrays
-DATA_PATH = os.path.join('Train_30word')
+DATA_PATH = os.path.join('Collect_Data')
 
 # Actions that we try to detect
 # array ของท่าทางภาษามือ
-actions = np.array(['wait', 'ขอโทษที่มาสาย', 'คุณชื่ออะไร', 'คุณสบายดีไหม', 'ฉันสบายดี',
-                    'แล้วพบกันใหม่', 'คุณกินข้าวแล้วหรือยัง', 'คุณอายุเท่าไหร่', 'ฉันกำลังจะกลับบ้าน', 'คุณจะกลับบ้านกี่โมง',
-                    'คุณเหนื่อยไหม', 'ฉันไม่สบาย', 'พรุ่งนี้คุณจะไปไหน', 'สวัสดี', 'ฉันกำลังทานข้าว',
-                    'ไปกินข้าวด้วยกันไหม', 'คุณทำงานอะไร', 'ฉันทำงานเป็นช่างทำผม', 'ฉันทำงานเป็นคนขับรถแท็กซี่', 'เมื่อวานไปไหนมา',
-                    'ฉันหิวข้าว', 'เมื่อวานฉันไปเที่ยว', 'ฉันไปหาหมอ', 'ฉันขอโทษ', 'ขอบคุณ',
-                    'ฉันไม่เป็นไร', 'โชคดี', 'วันนี้อากาศดี', 'ยินดีที่ได้รู้จัก', 'คุณเข้าใจไหม', 'ฉันไม่เข้าใจ'])
+actions = np.array(['wait', 'สวัสดี', 'ขอบคุณ', 'ยินดีที่ได้รู้จัก', 'ขอโทษที่มาสาย',
+                    'แล้วพบกันใหม่', 'โชคดี', 'คุณชื่ออะไร', 'คุณสบายดีไหม', 'ฉันสบายดี',
+                    'ฉันไม่สบาย', 'ฉันไม่เป็นไร', 'วันนี้อากาศดี', 'คุณกินข้าวแล้วหรือยัง', 'ไปกินข้าวด้วยกันไหม',
+                    'ฉันกำลังทานข้าว', 'พรุ่งนี้คุณจะไปไหน', 'ฉันกำลังจะกลับบ้าน', 'คุณจะกลับบ้านกี่โมง', 'คุณอายุเท่าไหร่',
+                    'คุณเหนื่อยไหม', 'ฉันหิวข้าว', 'คุณทำงานอะไร', 'ฉันทำงานเป็นช่างทำผม', 'ฉันทำงานเป็นคนขับรถแท็กซี่',
+                    'เมื่อวานไปไหนมา', 'เมื่อวานฉันไปเที่ยว', 'ฉันไปหาหมอ', 'ฉันขอโทษ', 'คุณเข้าใจไหม', 'ฉันไม่เข้าใจ'])
 
-# fifteen videos worth of data
-# 30 video = 30 folder
+# 30 videos worth of data
+# 30 folder = 30 frames
 no_sequences = 30
 
 # Videos are going to be 30 frames in length
-# 30 ภาพ ใน 1 วิดีโอ
+# ความยาวของ 1 เฟรม เท่ากับ 30 วินาที
 sequence_length = 30
 
 # Folder start
